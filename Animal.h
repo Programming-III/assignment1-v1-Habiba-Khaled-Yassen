98dcacb145185142f667eae196298eb39781a8d8
Habiba-Khaled-Yassen <habibayassen769@gmail.com>
2025-11-12 15:19:04 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -5,7 +5,37 @@
 using namespace std;
 
 //define animal class here 
+class Animal {
+    string name;
+    int age;
+    bool isHungry;
+public:
+    Animal(string n,int a) {
+        name =n;
+        age=a;
 
+    }
+    Animal() {
+        name="unknown";
+        age=0;
+    }
+    ~Animal(){}
+    string getName( );
+    int getAge() ;
+
+    void display();
+    void feed();
+
+};
+class Mammel: Animal{
+    string furColor;
+};
+class Bird: Animal{
+    float wingSpan;
+};
+class Reptile: Animal{
+    bool isVenomous;
+};
 
 
 #endif
